c6a0c3aca439a420b6516ad54326c4c6bb17c1ce
Youssef Ben Ayad <youssef1002016@gmail.com>
2025-04-22 20:55:59 +0100

    2ndTry

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,21 +1,11 @@
+/**
+ * Retrieves a list of students.
+ * @returns {{id: Number, firstName: String, location: String}[]}
+ */
 export default function getListStudents() {
-    const p1 = {
-      id: 1,
-      firstName: 'Guillaume',
-      location: 'San Francisco',
-    };
-  
-    const p2 = {
-      id: 2,
-      firstName: 'James',
-      location: 'Columbia',
-    };
-  
-    const p3 = {
-      id: 5,
-      firstName: 'Serena',
-      location: 'San Francisco',
-    };
-  
-    return [p1, p2, p3];
-  }
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+}
