87ce81be987990ecf95836b6854906b37f0a9066
Youssef Ben Ayad <youssef1002016@gmail.com>
2025-04-22 18:29:28 +0100

    0-test

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,17 +1,17 @@
 export default function getListStudents() {
-    let p1 = {
+    const p1 = {
         id: 1,
         firstName: 'Guillaume',
         location: 'San Francisco',
     };
 
-    let p2 = {
+    const p2 = {
         id: 2,
         firstName: 'James',
         location: 'Columbia',
     };
 
-    let p3 = {
+    const p3 = {
         id: 5,
         firstName: 'Serena',
         location: 'San Francisco',
