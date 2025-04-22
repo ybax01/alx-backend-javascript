9cf5a1d41e70d5d06fa38a4ad0ef7e10c1711d9e
Youssef Ben Ayad <youssef1002016@gmail.com>
2025-04-22 18:38:31 +0100

    one more

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -4,18 +4,18 @@ export default function getListStudents() {
       firstName: 'Guillaume',
       location: 'San Francisco',
     };
-
+  
     const p2 = {
       id: 2,
       firstName: 'James',
       location: 'Columbia',
     };
-
+  
     const p3 = {
       id: 5,
       firstName: 'Serena',
       location: 'San Francisco',
     };
-
-    return [p1, p2, p3];
-}
+  
+    return [o1, o2, o3];
+  }
